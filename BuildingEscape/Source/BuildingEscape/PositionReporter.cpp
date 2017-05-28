e97d83a808a9bdf0ac89558a090077409c3044ca
Phanto <phantomatik@hotmail.fr>
2017-05-28 21:13:22 +0200

    Updated

--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "BuildingEscape.h"
 #include "PositionReporter.h"
-
+#include "BuildingEscape.h"
+#include "Engine.h"
 
 // Sets default values for this component's properties
 UPositionReporter::UPositionReporter()
@@ -10,8 +10,6 @@ UPositionReporter::UPositionReporter()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -20,8 +18,8 @@ void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	FString ownerName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position Report for duty on %s"), *ownerName);
 }
 
 
